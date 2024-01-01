6434e57c88884967b97dd500b95031220d2d677a
Sumanta Singh <2758836s@student.gla.ac.uk>
2024-01-01 13:24:05 +0000

    first commit, only templates

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
-const canvasWidth = (ctx.width = 800);
-const canvasHeight = (ctx.height = 700);
+const canvasWidth = (canvas.width = 800);
+const canvasHeight = (canvas.height = 700);
